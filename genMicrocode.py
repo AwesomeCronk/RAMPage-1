348299a0af76e397a64fa0f570c96f44bc316a0a
Clayton Cronk <awesomecronk@gmail.com>
2022-05-13 22:34:02 -0500

    Switched from Logical to Digital. Now can execute programs of only CPR instructions.

---
+++ genMicrocode.py
@@ -0,0 +1,87 @@
+controlLines = [
+    'MP_In',
+    'MA_In',
+    'MD_In',
+    'MD_Out',
+    'AR_Sel',
+    'AR_In',
+    'AR_Out',
+    'PC_Inc',
+    'PC_In',
+    'PC_Out',
+    '',
+    'SC_Res',
+    'IR_In',
+    'IR_Out',
+    '',
+    ''
+]
+steps = 8
+
+fetch = [
+    ['PC_Out', 'MA_In'],
+    ['MD_Out', 'IR_In']
+]
+
+SET = [
+    *fetch,
+    [],
+    [],
+    [],
+    [],
+    [],
+    []
+]
+CPR = [
+    *fetch,
+    ['IR_Out', 'MA_In'],
+    ['MD_Out', 'AR_In'],
+    ['PC_Inc', 'SC_Res'],
+    [],
+    [],
+    []
+]
+CPM = [
+    *fetch,
+    [],
+    [],
+    [],
+    [],
+    [],
+    []
+]
+JMP = [
+    *fetch,
+    [],
+    [],
+    [],
+    [],
+    [],
+    []
+]
+
+instructions = {'SET': SET, 'CPR': CPR, 'CPM': CPM, 'JMP': JMP}
+
+if __name__ == '__main__':
+    binary = []
+    for instructionName in instructions.keys():
+        print(instructionName)
+        instruction = instructions[instructionName]
+        instructionBinary = []
+        for s, step in enumerate(instruction):
+            stepBinary = [0] * len(controlLines)
+            for line in step:
+                assert line in controlLines
+                stepBinary[controlLines.index(line)] = 1
+            print('{}: {} - {}'.format(s, ', '.join(step).ljust(20), ''.join([str(bit) for bit in stepBinary])))
+            instructionBinary += stepBinary
+        binary += instructionBinary
+
+
+    with open('Microcode.bin', 'wb') as microcodeFile:
+        number = 0
+        for bit in binary:
+            number *= 2
+            number += bit
+        microcodeFile.write(int.to_bytes(number, len(binary) // 8, 'big'))
+        print(bytearray(int.to_bytes(number, len(binary) // 8, 'big')).hex())
